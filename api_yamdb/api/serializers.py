4c19570d31f1829b11935edb0d1e27384ff7d003
Василий Вихляев <vikhlyaev.vasiliy@yandex.ru>
2022-06-01 21:27:23 +0700

    USERS

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -30,3 +30,30 @@ class RegisterNewUserSerializer(serializers.ModelSerializer):
 class TokenSerializer(serializers.Serializer):
     username = serializers.CharField()
     confirmation_code = serializers.CharField()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        validators=[
+            UniqueValidator(queryset=User.objects.all())
+        ],
+        required=True,
+    )
+    email = serializers.EmailField(
+        validators=[
+            UniqueValidator(queryset=User.objects.all())
+        ]
+    )
+
+    class Meta:
+        fields = ("username", "email", "first_name",
+                  "last_name", "bio", "role")
+        model = User
+
+
+class UserEditSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ("username", "email", "first_name",
+                  "last_name", "bio", "role")
+        model = User
+        read_only_fields = ('role',)
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-from .views import get_jwt_token, register_new_user
+from .views import UserViewSet, get_jwt_token, register_new_user
+
+v1_router = DefaultRouter()
+v1_router.register(r'users', UserViewSet)
 
 urlpatterns = [
+    path('v1/', include(v1_router.urls)),
     path('v1/auth/signup/', register_new_user, name='register_new_user'),
     path('v1/auth/token/', get_jwt_token, name='token')
 ]
